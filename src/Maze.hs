b43ca82b12bf7031290816aac411a20b4a2e3e3a
Dojo <dojo@daniel-SVD11215CBB.(none)>
2013-03-20 20:13:11 -0300

    Algumacoisa

--- src/Maze.hs
+++ src/Maze.hs
@@ -2,25 +2,25 @@ module Maze (
     Maze
   ) where
 
-import Data.Map
+import qualified Data.Map as M
+import Data.Map (Map)
 
 data Cell = Cell {
     southWall :: Bool
   , eastWall  :: Bool	
   } 
 
-data Maze = Maze Size Map
+data Maze = Maze Size (Map Position Cell) 
 
 type Position = (Int, Int)
 
 type Size = (Int, Int)
 
 empty :: Size -> Maze
-empty = undefined
+empty sz = Maze 	 
 
 data Direction = N | S | E | W deriving (Eq, Ord, Show, Read, Enum)
 
 canMoveTo :: Maze -> Position -> Direction -> Bool
 canMoveTo m p d = False
-
 
