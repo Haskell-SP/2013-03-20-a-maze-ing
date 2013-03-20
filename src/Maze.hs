a52c12120cc9e8aadcef0d2919299a15e53e3dc4
Dojo <dojo@daniel-SVD11215CBB.(none)>
2013-03-20 20:58:08 -0300

    move test

--- src/Maze.hs
+++ src/Maze.hs
@@ -30,10 +30,10 @@ canMoveTo _ (0, _) W = False
 canMoveTo _ (_, 0) N = False
 canMoveTo m (_, y) E | fst (size m) + 1 == y = False
 canMoveTo m (x, _) S | snd (size m) + 1 == x = False
-canMoveTo m p S = case M.lookup p m of
+canMoveTo m p S = case M.lookup p (topology m) of
 					  Just c  -> not $ southWall c
 					  Nothing -> True
-canMoveTo m p E = case M.lookup p m of
+canMoveTo m p E = case M.lookup p (topology m) of
 					  Just c  -> not $ eastWall c
 					  Nothing -> True
 canMoveTo m p N = canMoveTo m (move N p) S
--- tests/Main.hs
+++ tests/Main.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
 import Test.Hspec
+import Test.Hspec.QuickCheck
 import Maze
 
 main :: IO ()
@@ -9,5 +10,7 @@ main = hspec $ do
     it "compiles!" (return () :: IO ())
 
   describe "move" $ do
-  	it "can move north" $ do
-  		move N (1, 1) `shouldBe` (1, 0)
+    it "can move north" $ do
+      move N (1, 1) `shouldBe` (1, 0)
+    prop "going north then south leads to the same place" $ 
+      \p -> move S (move N p) == p
