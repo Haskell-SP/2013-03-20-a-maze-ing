39d59e773b06bd85e62f685c01f74b78059d109e
Dojo <dojo@daniel-SVD11215CBB.(none)>
2013-03-20 20:07:30 -0300

    Progress!

--- src/Maze.hs
+++ src/Maze.hs
@@ -1,1 +1,26 @@
-module Maze where
+module Maze (
+    Maze
+  ) where
+
+import Data.Map
+
+data Cell = Cell {
+    southWall :: Bool
+  , eastWall  :: Bool	
+  } 
+
+data Maze = Maze Size Map
+
+type Position = (Int, Int)
+
+type Size = (Int, Int)
+
+empty :: Size -> Maze
+empty = undefined
+
+data Direction = N | S | E | W deriving (Eq, Ord, Show, Read, Enum)
+
+canMoveTo :: Maze -> Position -> Direction -> Bool
+canMoveTo m p d = False
+
+
