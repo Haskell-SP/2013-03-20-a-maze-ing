3b3fbc6fb6abd5be0e5c8627c575bfb761c07266
Daniel Yokomizo <daniel.yokomizo@gmail.com>
2013-03-21 09:51:41 -0300

    Formatting

--- src/Maze.hs
+++ src/Maze.hs
@@ -6,10 +6,11 @@ module Maze {-(
 import qualified Data.Map as M
 import Data.Map (Map)
 
+
 data Cell = Cell {
     southWall :: Bool
   , eastWall  :: Bool
-  } 
+  }
 
 data Maze = Maze
   { size :: Size
@@ -20,10 +21,13 @@ type Position = (Int, Int)
 
 type Size = (Int, Int)
 
+
 empty :: Size -> Maze
 empty = flip Maze M.empty 
 
+
 data Direction = N | S | E | W deriving (Eq, Ord, Show, Read, Enum, Bounded)
+
 
 canMoveTo :: Maze -> Position -> Direction -> Bool
 canMoveTo _ (0, _) W = False
@@ -39,16 +43,19 @@ canMoveTo m p E = case M.lookup p (topology m) of
 canMoveTo m p N = canMoveTo m (move N p) S
 canMoveTo m p W = canMoveTo m (move W p) E
 
+
 move :: Direction -> Position -> Position
 move W (x, y) = (x - 1, y)
 move E (x, y) = (x + 1, y)
 move N (x, y) = (x, y - 1)
 move S (x, y) = (x, y + 1)
 
+
 opposite N = S
 opposite S = N
 opposite E = W
 opposite W = E
+
 
 insertWall :: Maze -> Position -> Direction -> Maze
 insertWall m p d 
